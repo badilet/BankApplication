abbd16e8bd24114d7e9a98a3d78c0404ffbd7787
Adilet Bekboev <badilet@gmail.com>
2022-10-10 10:21:59 -0500

    commit 3

--- src/main/java/test_package_git/GitTestClass.java
+++ src/main/java/test_package_git/GitTestClass.java
@@ -3,5 +3,6 @@ package test_package_git;
 public class GitTestClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hi there");
     }
 }
